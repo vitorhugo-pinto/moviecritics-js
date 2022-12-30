ceaab49c5982d6348931dd79c2eacf0a721fc0bc
Vitor Hugo Pinto <vhpinto1@gmail.com>
2022-12-30 13:20:10 -0300

    Funcionalidade de deslogar usuário

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,14 @@
 import { Avatar } from './Avatar';
+import { SignOut } from 'phosphor-react';
+
 
 import styles from './Sidebar.module.css';
 
-export function Sidebar() {
+export function Sidebar({user, onLogOut}) {
+    function handleLogOut(){
+        onLogOut();
+    }
+
     return (
         <aside className={styles.sidebar}>
             <img 
@@ -13,9 +19,15 @@ export function Sidebar() {
                 <Avatar 
                     src='https://images.unsplash.com/photo-1633613286991-611fe299c4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=40'
                 />
-                <strong>Vitor Hugo</strong>
-                <span>vitor@email.com</span>
+                <strong>{user.name}</strong>
+                <span>{user.email}</span>
             </div>
+            <footer>
+                <a onClick={handleLogOut} href='#'>
+                    <SignOut size={20} />
+                    Log out
+                </a>
+            </footer>
         </aside>
     );
 }
