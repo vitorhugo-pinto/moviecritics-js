098017c6d9c359aba100b9ec2cf410ca8837ad76
Vitor Hugo Pinto <vhpinto1@gmail.com>
2022-12-30 13:35:44 -0300

    Adicionado icone de favorito

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './Movie.module.css';
 import { Poster } from './Poster';
+import { Star } from 'phosphor-react';
 
 export function Movie({id, title, year, poster}) {
     const navigate = useNavigate();
@@ -15,6 +16,11 @@ export function Movie({id, title, year, poster}) {
                 <div className={styles.content}>
                     <Poster src={poster} />
                     <div className={styles.contentInfo}>
+                        <header>
+                            <a href='#'>
+                                <Star size={20} />
+                            </a>
+                        </header>
                         <strong>{title}</strong>
                         <span>{year}</span>
                         <button onClick={() => toDetails(id)}>Show more</button>
